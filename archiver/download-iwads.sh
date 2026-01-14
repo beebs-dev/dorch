b77c1dc9f4e49db572f0ed1f7cc977561669d45f
Thomas Havlik <thavlik@protonmail.com>
2026-01-14 15:19:03 -0500

    ...

--- archiver/download-iwads.sh
+++ archiver/download-iwads.sh
@@ -2,3 +2,4 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 # TODO: aws s3 ls the bucket, try to get from redis, fallback to s3, cache in redis (if necessary), etc.
+# s3://$IWAD_BUCKET should be downloaded locally to $IWADS_DIR 
