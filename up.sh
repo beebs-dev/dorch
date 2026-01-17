df4eda51c60d737d065c3b940a7310506404b702
Thomas Havlik <thavlik@protonmail.com>
2026-01-17 18:06:20 -0500

    ...

--- up.sh
+++ up.sh
@@ -20,31 +20,21 @@ do_build() {
 }
 do_restart() {
     restart_args=()
-    restart_app=$([ "${#restart_args[@]}" -eq 0 ])
-    restart_server=$([ "${#restart_args[@]}" -eq 0 ])
     for arg in "$@"; do
         case "$arg" in
         party)
             restart_args+=("party")
             restart_args+=("party-router")
             ;;
-        client)
-            restart_app=true
-            ;;
         server|proxy)
             restart_server=true
+            kubectl delete pod --context $KUBECONTEXT -n $NAMESPACE test-game
             ;;
         *)
             restart_args+=("$arg")
             ;;
         esac
     done
-    if [ "$restart_app" = true ]; then
-        kubectl rollout restart deployment --context $KUBECONTEXT -n apps apps-zandronum
-    fi
-    if [ "$restart_server" = true ]; then
-        kubectl delete pod --context $KUBECONTEXT -n $NAMESPACE test-game
-    fi
     kubectl rollout restart deployment --context $KUBECONTEXT -n $NAMESPACE "${restart_args[@]/#/$NAMESPACE-}"
 }
 main() {
