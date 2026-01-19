bcff0539206c7c57ab6cd13754fa4a9983d9475c
Thomas Havlik <thavlik@protonmail.com>
2026-01-19 18:18:20 -0500

    ...

--- wadinfo/src/sql/featured_wads.sql
+++ wadinfo/src/sql/featured_wads.sql
@@ -6,6 +6,7 @@ from wads w
 where w.hidden = false
   and w.can_download = true
   and exists (select 1 from wad_map_images i where i.wad_id = w.wad_id)
+  and exists (select 1 from wad_analysis a where a.wad_id = w.wad_id)
 order by
   (nullif(trim(w.meta_json->>'title'), '') is null) asc,
   random()
