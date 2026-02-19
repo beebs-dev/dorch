8731f9c9573183758b1d205c94da0fbff740cae9
Tom Havlik <thavlik@protonmail.com>
2026-02-18 21:05:32 -0500

    ...

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -14,6 +14,13 @@ export POSTGRES_PASSWORD=$(secret password)
 export POSTGRES_PORT=$(secret port)
 export POSTGRES_DB=$(secret database)
 export POSTGRES_SSL_MODE=$(secret sslmode)
-docker run -it --rm postgres:18 \
-    psql "host=$POSTGRES_HOST port=$POSTGRES_PORT user=$POSTGRES_USER password=$POSTGRES_PASSWORD dbname=$POSTGRES_DB sslmode=$POSTGRES_SSL_MODE" \
-    $@
+docker run --rm \
+    -e PGPASSWORD="$POSTGRES_PASSWORD" \
+    -e PGSSLMODE="$POSTGRES_SSL_MODE" \
+    postgres:18 \
+    pg_dump \
+        -h "$POSTGRES_HOST" \
+        -p "$POSTGRES_PORT" \
+        -U "$POSTGRES_USER" \
+        -d dorch \
+    > dorch.sql
